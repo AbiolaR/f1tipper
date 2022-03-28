5b5f0eaa6bb09618c5bdc9a043e1f10808f19ae5
Abiola Rasheed <rasheedabi97@hotmail.com>
2022-03-29 00:19:15 +0200

    visual admin control button improvements - cleanup

--- src/main/angular/src/app/component/bet/bet.component.ts
+++ src/main/angular/src/app/component/bet/bet.component.ts
@@ -22,6 +22,7 @@ import { BetItemTypeGroup } from 'src/app/model/enum/bet-item-type-group';
 export class BetComponent implements OnInit {
 
   BetItemTypeGroup = BetItemTypeGroup
+  BetDataType = BetDataType
   isAdmin = false
   private betId: string | null | undefined;
   bet: Bet | undefined;
@@ -37,29 +38,8 @@ export class BetComponent implements OnInit {
     this.isAdmin = this.userService.isAdmin()
     this.betId = this.activatedRoute.snapshot.paramMap.get("id");
     this.betService.getBet(this.betId).subscribe({      
-      next: (data) => {this.bet = data
-      console.log(this.bet)}
+      next: (data) => this.bet = data
     })
-  }
-
-  openQualifyingDialog() {
-    this.openBetItemDialog(BetDataType.QUALIFYING);
-  }
-
-  openRaceDialog() {
-    this.openBetItemDialog(BetDataType.RACE);
-  }
-
-  openDNFDialog() {
-    this.openBetItemDialog(BetDataType.DNF);
-  }
-
-  openDriverDialog() {
-    this.openBetItemDialog(BetDataType.DRIVER);
-  }
-
-  openConstructorDialog() {
-    this.openBetItemDialog(BetDataType.CONSTRUCTOR);
   }
 
   openBetItemDialog(type: BetDataType) {
@@ -79,32 +59,5 @@ export class BetComponent implements OnInit {
       }
     });
   }
-
-  /*updateResult(typeGroup: BetItemTypeGroup, button: any) {
-    button.classList.add('loading');
-    this.resultService.triggerResultUpdate(this.bet!!.raceId, typeGroup).subscribe(result => {
-      button.classList.remove('loading');
-      this.handleResult(result);
-    });
-  }
-
-  handleResult(result: boolean) {
-    let message = 'oh oh, something went wrong';
-    let action = '';
-    if (result) {
-      message = 'successful';
-      action = 'reload'
-    }
-    let snackBarRef = this.snackBar.open(message, action, {duration: 2500, verticalPosition: 'bottom'});
-    snackBarRef.onAction().subscribe(() => {
-      window.location.reload();
-    });
-  }
-
-  calculatePoints(typeGroup: BetItemTypeGroup) {
-    this.resultService.calculatePoints(this.bet!!.raceId, typeGroup).subscribe(result => {
-      this.handleResult(result);
-    });
-  }*/
 
 }
