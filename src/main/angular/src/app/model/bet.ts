a99c2e2666b616000115325b7c6e7fc5f60e9044
Abiola Rasheed <rasheedabi97@hotmail.com>
2022-04-08 12:46:11 +0200

    removed unneded properties of bet

--- src/main/angular/src/app/model/bet.ts
+++ src/main/angular/src/app/model/bet.ts
@@ -2,16 +2,8 @@ import { Race } from "./race";
 
 export interface Bet {
     id: number;
-    /*title: string;
-    name: string;
-    status: string;
-    trackSvg: string;
-    flagImageUrl: string;*/
     type: string;
     points: number;
     status: string;
-    /*dateRange: string;
-    raceId: number;
-    country: number;*/
     race: Race
 }
