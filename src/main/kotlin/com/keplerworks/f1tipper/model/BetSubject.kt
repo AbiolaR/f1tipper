670eea7fc075905ac2ffdf125a8211e5f8f1f6cb
Abiola Rasheed <rasheedabi97@hotmail.com>
2022-04-08 12:45:01 +0200

    ignore field races on betsubject when building json

--- src/main/kotlin/com/keplerworks/f1tipper/model/BetSubject.kt
+++ src/main/kotlin/com/keplerworks/f1tipper/model/BetSubject.kt
@@ -1,5 +1,6 @@
 package com.keplerworks.f1tipper.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.keplerworks.f1tipper.type.BetSubjectType
 import javax.persistence.*
 
@@ -18,5 +19,6 @@ class BetSubject (
         joinColumns = [JoinColumn(name = "bet_subject_id")],
         inverseJoinColumns = [JoinColumn(name = "race_id")]
     )
+    @JsonIgnore
     val races: Set<Race> = setOf()
 )
